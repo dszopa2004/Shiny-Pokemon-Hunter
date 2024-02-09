4f75d9a6a89617d12161a2d49b77f33060b9e936
SpiceySauce <55515534+SpiceySauce@users.noreply.github.com>
2024-02-08 19:05:34 -0600

    Add movement function and rattata image
    Adds a movement function and a rattata image

--- main.py
+++ main.py
@@ -4,10 +4,36 @@ import keyboard
 from time import time, sleep
 
 # Once 's' is pressed, program starts
-print("Press 's'' to start the program.")
-print("After pressing 's', press 'q' to stop the program.")
-keyboard.wait('s')
+print("Press 'k' to start the program.")
+print("After pressing 'k', press 'q' to stop the program.")
+keyboard.wait('k')
+print("Starting the program...")
+
+# Movement function for player
+# Currently not in use
+
+
+def move_character():
+    sleep(1)
+    pyautogui.keyDown('left')
+    sleep(2)
+    pyautogui.keyUp('left')
+
+    pyautogui.keyDown('right')
+    sleep(2)
+    pyautogui.keyUp('right')
+
 
 while True:
+    sleep(1)
+    pyautogui.keyDown('left')
+    sleep(2)
+    pyautogui.keyUp('left')
+
+    pyautogui.keyDown('right')
+    sleep(2)
+    pyautogui.keyUp('right')
+
     if keyboard.is_pressed('q'):
+        print("Stopping program.")
         break
