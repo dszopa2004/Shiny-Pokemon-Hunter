2425697517611c8dd0069a7aa75dc5d95f9d23da
SpiceySauce <55515534+SpiceySauce@users.noreply.github.com>
2024-02-08 20:22:16 -0600

    Add movement script
    Character now moves around until the battle_image is detected. Exits the
    program after the battle is found

--- main.py
+++ main.py
@@ -24,12 +24,8 @@ def find_and_click(template, screen, threshold=0.9):
 
     # Check if the match is above the threshold
     if max_val > threshold:
-        # Calculate the center of the template match location
-        center_x = max_loc[0] + template.shape[1] // 2
-        center_y = max_loc[1] + template.shape[0] // 2
-        pyautogui.click(center_x, center_y)
-        print(center_x)
-        print(center_y)
+        print("Battle found!")
+        return True
 
 # Movement function for player
 # Currently not in use
@@ -51,7 +47,13 @@ def main():
 
     while True:
         screen = capture_screen()
-        find_and_click(battle_image, screen)
+        found_battle = find_and_click(battle_image, screen)
+
+        if not found_battle:
+            move_character()
+        else:
+            print("Found battle, exiting program")
+            break
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print("Stopping program.")
